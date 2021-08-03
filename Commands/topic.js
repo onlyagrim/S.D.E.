56732aa39fa3ac42719ca13b75fdb9788d539e4c
Bhuvnesh875 <bhuvnesh875@gmail.com>
2021-08-03 14:16:34 +0530

    Updated Topics

--- Commands/topic.js
+++ Commands/topic.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const Embed1 = new Discord.MessageEmbed()
+const Embed3 = new Discord.MessageEmbed()
 	.setColor('0x0099ff')
 
 	.setAuthor(
@@ -10,7 +10,7 @@ const Embed1 = new Discord.MessageEmbed()
 	.addFields(
 		{
 			name:'Data Structures:',
-            value:'`SinglyLL\n` `DoublyLL\n` `CircularLL\n` `Stack\n` `SimpleQueue\n` `CircularQueue\n` `PriorityQueue\n` `DoubleEndedQueue\n` '
+            value:'`SinglyLL\n` `DoublyLL\n` `CircularLL\n` `StackByArray\n` '
 		},
 		
 	)
@@ -22,4 +22,4 @@ const Embed1 = new Discord.MessageEmbed()
 		'https://i.ibb.co/jG3Gffy/dsa.jpg'
 	);
 
-module.exports = Embed1;
+module.exports = Embed3;
