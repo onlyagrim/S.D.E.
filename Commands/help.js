3129a669e9dd22dc19864533f5925662a80f5455
Bhuvnesh875 <bhuvnesh875@gmail.com>
2021-08-03 17:07:17 +0530

    contains bot command Embed

--- Commands/help.js
+++ Commands/help.js
@@ -22,7 +22,7 @@ const Embed1 = new Discord.MessageEmbed()
 		{ name: 'Fun Commands:' ,value: '`+MeMe\n`' },
 		{
 			name: ' Admin Commands:',
-			value: '`+cc\n` `+status\n\n\n`'
+			value: '`+cc or +clearchannel\n` `+status\n\n\n`'
 		}
 	)
 	.setTimestamp()
