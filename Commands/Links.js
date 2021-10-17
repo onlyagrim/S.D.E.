d8208254ff0228c3df36bd3d52875d154a96387d
Saurabh Sharma <saurabh182000@gmail.com>
2021-10-17 19:36:04 +0530

    Updated

--- Commands/Links.js
+++ Commands/Links.js
@@ -23,6 +23,13 @@ function sendLink(message){
         else if (args[1] === 'DoublyLL' && args[3] === 'Java') {
             message.channel.send("Doubly Linked List in Java", { files: ["./Codes/DoublyLLJava.txt"] });
         }
+        else if (args[1] === 'LinearSearch' && args[3] === 'Java') {
+            message.channel.send("Linear Search in Java", { files: ["./Codes/LinearSearchJava.txt"] });
+        }
+        else if (args[1] === 'BinarySearch' && args[3] === 'Java') {
+            message.channel.send("Binary Search in Java", { files: ["./Codes/BinarySearchJava.txt"] });
+        }
+
 
 }
 
