45fa2aebab6dd68f3483caf1be083756624b0db4
Bhuvnesh Sharma <bhuvnesh875@gmail.com>
2022-02-20 00:17:01 +0530

    feature test

--- Commands/Meme.js
+++ Commands/Meme.js
@@ -1,6 +1,8 @@
 const fetch = require("node-fetch");
 const env = require("dotenv");
 const Discord = require("discord.js");
+const RedditImageFetcher = require("reddit-image-fetcher");
+
 module.exports = async function (message) {
   const categories = [
     "Relationship",
@@ -19,6 +21,12 @@ module.exports = async function (message) {
   const url = `https://api.humorapi.com/memes/random?api-key=${process.env.API_KEY}&keywords=${random},`;
   const response = await fetch(url);
   const json = await response.json();
+
+  // RedditImageFetcher.fetch({
+  //   type: "meme",
+  // }).then((result) => {
+  //   message.channel.send(result[0].image);
+  // });
 
   message.channel.send(json.url);
 };
