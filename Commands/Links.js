6b507427e1fc276b9a6d25d834f75d6d2ad95d09
Bhuvnesh Sharma <bhuvnesh875@gmail.com>
2022-02-20 00:16:31 +0530

    Added new links

--- Commands/Links.js
+++ Commands/Links.js
@@ -1,7 +1,19 @@
 function sendLink(message) {
   const [...args] = message.content.split(/\s+/);
 
-  if (args[1] === "SinglyLL" && args[3] === "C++") {
+  if (args[1] === "Array" && args[3] === "Cpp") {
+    message.channel.send("Array in Cpp", {
+      files: ["./Codes/ArraysCpp.txt"],
+    });
+  } else if (args[1] === "CircularLL" && args[3] === "Cpp") {
+    message.channel.send("Circular Linked List in Cpp", {
+      files: ["./Codes/CircularLLCpp.txt"],
+    });
+  } else if (args[1] === "CircularDoublyLL" && args[3] === "Cpp") {
+    message.channel.send("Circular Linked List in Cpp", {
+      files: ["./Codes/CircularDoublyLLCpp.txt"],
+    });
+  } else if (args[1] === "SinglyLL" && args[3] === "Cpp") {
     message.channel.send("Singly Linked List in Cpp", {
       files: ["./Codes/SinglyLLCpp.txt"],
     });
@@ -13,7 +25,7 @@ function sendLink(message) {
     message.channel.send("Singly Linked List in Java", {
       files: ["./Codes/SinglyLLJava.txt"],
     });
-  } else if (args[1] === "DoublyLL" && args[3] === "C++") {
+  } else if (args[1] === "DoublyLL" && args[3] === "Cpp") {
     message.channel.send("Doubly Linked List in Cpp", {
       files: ["./Codes/DoublyLLCpp.txt"],
     });
@@ -25,11 +37,59 @@ function sendLink(message) {
     message.channel.send("Doubly Linked List in Java", {
       files: ["./Codes/DoublyLLJava.txt"],
     });
+  } else if (args[1] === "QueueUsingArray" && args[3] === "Cpp") {
+    message.channel.send("Queue Using Array in Cpp", {
+      files: ["./Codes/QueueUsingArrayCpp.txt"],
+    });
+  } else if (args[1] === "QueueUsingArray" && args[3] === "Java") {
+    message.channel.send("Queue Using Array in Java", {
+      files: ["./Codes/QueueUsingArrayJava.txt"],
+    });
+  } else if (args[1] === "QueueUsingArray" && args[3] === "Python") {
+    message.channel.send("Queue Using Array in Python", {
+      files: ["./Codes/QueueUsingArrayPython.txt"],
+    });
+  } else if (args[1] === "QueueUsingLL" && args[3] === "Cpp") {
+    message.channel.send("Queue Using Linked list in Cpp", {
+      files: ["./Codes/QueueUsingLLCpp.txt"],
+    });
+  } else if (args[1] === "QueueUsingLL" && args[3] === "Java") {
+    message.channel.send("Queue Using Linked list in Java", {
+      files: ["./Codes/QueueUsingLLJava.txt"],
+    });
+  } else if (args[1] === "QueueUsingLL" && args[3] === "Python") {
+    message.channel.send("Queue Using Linked List in Python", {
+      files: ["./Codes/QueueUsingLLPython.txt"],
+    });
+  } else if (args[1] === "StackUsingArray" && args[3] === "Cpp") {
+    message.channel.send("Stack Using Array in Cpp", {
+      files: ["./Codes/StackUsingArrayCpp.txt"],
+    });
+  } else if (args[1] === "StackUsingArray" && args[3] === "Java") {
+    message.channel.send("Stack Using Array in Java", {
+      files: ["./Codes/StackUsingArrayJava.txt"],
+    });
+  } else if (args[1] === "StackUsingArray" && args[3] === "Python") {
+    message.channel.send("Stack Using Array in Python", {
+      files: ["./Codes/StackUsingArrayPython.txt"],
+    });
+  } else if (args[1] === "StackUsingLL" && args[3] === "Cpp") {
+    message.channel.send("Stack Using Linked list in Cpp", {
+      files: ["./Codes/StackUsingLLCpp.txt"],
+    });
+  } else if (args[1] === "StackUsingLL" && args[3] === "Java") {
+    message.channel.send("Stack Using Linked list in Java", {
+      files: ["./Codes/StackUsingLLJava.txt"],
+    });
+  } else if (args[1] === "StackUsingLL" && args[3] === "Python") {
+    message.channel.send("Stack Using Linked List in Python", {
+      files: ["./Codes/StackUsingLLPython.txt"],
+    });
   } else if (args[1] === "LinearSearch" && args[3] === "Java") {
     message.channel.send("Linear Search in Java", {
       files: ["./Codes/LinearSearchJava.txt"],
     });
-  } else if (args[1] === "LinearSearch" && args[3] === "C++") {
+  } else if (args[1] === "LinearSearch" && args[3] === "Cpp") {
     message.channel.send("Linear Search in Cpp", {
       files: ["./Codes/LinearSearchCpp.txt"],
     });
@@ -41,7 +101,7 @@ function sendLink(message) {
     message.channel.send("Binary Search in Java", {
       files: ["./Codes/BinarySearchJava.txt"],
     });
-  } else if (args[1] === "BinarySearch" && args[3] === "C++") {
+  } else if (args[1] === "BinarySearch" && args[3] === "Cpp") {
     message.channel.send("Binary Search in Cpp", {
       files: ["./Codes/BinarySearchCpp.txt"],
     });
@@ -50,8 +110,16 @@ function sendLink(message) {
       files: ["./Codes/BinarySearchPython.txt"],
     });
   } else if (args[1] === "SearchInMatrix" && args[3] === "Java") {
-    message.channel.send("Search Element in Matrix", {
+    message.channel.send("Search Element in Matrix Java", {
       files: ["./Codes/SearchInMatrixJava.txt"],
+    });
+  } else if (args[1] === "SearchInMatrix" && args[3] === "Cpp") {
+    message.channel.send("Search Element in Matrix Cpp", {
+      files: ["./Codes/SearchInMatrixCpp.txt"],
+    });
+  } else if (args[1] === "SearchInMatrix" && args[3] === "Python") {
+    message.channel.send("Search Element in Matrix Python", {
+      files: ["./Codes/SearchInMatrixPython.txt"],
     });
   } else {
     message.reply(
